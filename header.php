a822dc878594f198bc886d03ad949406927b7858
lailahgrant <lailahgrant19@gmail.com>
2022-02-13 23:38:49 +0300

    added the project to heroku
    added a composer.json file, ran composer install

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kabaka Electronics</title>
+    <title>Electronics Website</title>
 
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -52,7 +52,7 @@
         <!-- start primary navigation -->
         <nav class="navbar navbar-expand-lg navbar-dark color-primary-bg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="./index.php">Kabaka Electronics</a>
+                <a class="navbar-brand" href="./index.php">Electronics Website</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                     aria-label="Toggle navigation">
