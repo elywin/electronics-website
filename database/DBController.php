631b2fdc3b2312bbca14b0d0320a479bf47e7e38
lailahgrant <lailahgrant19@gmail.com>
2022-02-14 00:17:49 +0300

    .gitignore for vendor
    trying to connect heroku to phpmyadmin in code
    
    errors just

--- database/DBController.php
+++ database/DBController.php
@@ -1,26 +1,42 @@
 <?php
 class DBController {
     //database connection properties
-    protected $host = 'localhost';
+    /*protected $host = 'localhost';
     protected $user = 'root';
     protected $password = '';
-    protected $database = 'electronics';
+    protected $database = 'electronics';*/
+
+    //Get Heroku ClearDB connection information
+    protected $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    protected $cleardb_server = $cleardb_url["host"];
+    protected $cleardb_username = $cleardb_url["user"];
+    protected $cleardb_password = $cleardb_url["pass"];
+    protected $cleardb_db = substr($cleardb_url["path"],1);
+    protected $active_group = 'default';
+    protected $query_builder = TRUE;
     
     
     // Connect to DB
-    //create a constructor to initialize the connection f the database
+    //create a constructor to initialize the connection of the database
     //connection property
     public $con = null;
     
     
     //call constructor
     public function __construct(){
-        $this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+        /*$this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
        
         if($this->con->connect_error){
             echo "Fail" .$this->con->connect_error;        
-        }
+        }*/
         //echo "Connection successfull";
+
+        $this->conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    if($this->conn->connect_error){
+            echo "Fail" .$this->conn->connect_error;        
+        }
+        echo "Connection successfull";
+        
     }
     
 
@@ -35,6 +51,17 @@ class DBController {
         $this->con = null;
     }
 }
+
+
+    public $conn = null;
+    //method for connecting heroku clearDB
+    protected function clearDatabase(){
+        
+    // Connect to DB
+    
+    }
+    
+        
     
 } //close of class
 
